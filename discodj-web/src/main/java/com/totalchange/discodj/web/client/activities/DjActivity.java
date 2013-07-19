dee51ae58d3652b25f199997b2985713a5cfc9e0
KolonelKustard <ralph@totalchange.com>
2013-07-19 19:40:49 +0100

    Wired up place changes to activity for DJ

--- discodj-web/src/main/java/com/totalchange/discodj/web/client/activities/DjActivity.java
+++ discodj-web/src/main/java/com/totalchange/discodj/web/client/activities/DjActivity.java
@@ -1,5 +1,6 @@
 package com.totalchange.discodj.web.client.activities;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -9,8 +10,14 @@ import net.customware.gwt.dispatch.client.DispatchAsync;
 
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.place.shared.PlaceController;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.totalchange.discodj.web.client.error.ErrorHandler;
+import com.totalchange.discodj.web.client.places.DjPlace;
 import com.totalchange.discodj.web.client.views.DjView;
+import com.totalchange.discodj.web.shared.dj.SearchAction;
+import com.totalchange.discodj.web.shared.dj.SearchResult;
 import com.totalchange.discodj.web.shared.player.Media;
 
 public class DjActivity extends AbstractActivity implements DjView.Presenter {
@@ -18,23 +25,49 @@ public class DjActivity extends AbstractActivity implements DjView.Presenter {
             .getName());
 
     private DjView djView;
+    private PlaceController placeController;
     private DispatchAsync dispatchAsync;
-    
+    private ErrorHandler errorHandler;
+
     private String keywords = null;
     private List<String> facetIds = null;
 
     @Inject
-    public DjActivity(DjView djView, DispatchAsync dispatchAsync) {
+    public DjActivity(DjView djView, PlaceController placeController,
+            DispatchAsync dispatchAsync, ErrorHandler errorHandler) {
         this.djView = djView;
+        this.placeController = placeController;
         this.dispatchAsync = dispatchAsync;
+        this.errorHandler = errorHandler;
 
         this.djView.setPresenter(this);
     }
-    
+
+    private void showSearchResults(SearchResult result) {
+
+    }
+
+    private void search() {
+        SearchAction action = new SearchAction();
+        action.setKeywords(keywords);
+        action.setFacetIds(facetIds);
+        dispatchAsync.execute(action, new AsyncCallback<SearchResult>() {
+            @Override
+            public void onFailure(Throwable caught) {
+                errorHandler.loadingError(caught);
+            }
+
+            @Override
+            public void onSuccess(SearchResult result) {
+                showSearchResults(result);
+            }
+        });
+    }
+
     public void setKeywords(String keywords) {
         this.keywords = keywords;
     }
-    
+
     public void setFacetIds(List<String> facetIds) {
         this.facetIds = facetIds;
     }
@@ -43,6 +76,12 @@ public class DjActivity extends AbstractActivity implements DjView.Presenter {
     public void start(AcceptsOneWidget container, EventBus eventBus) {
         logger.finer("Starting up InitJizzActivity");
         container.setWidget(djView.asWidget());
+
+        if ((keywords != null && keywords.length() > 0)
+                || (facetIds != null && !facetIds.isEmpty())) {
+            search();
+        }
+
         logger.finer("Finished starting up InitJizzActivity");
     }
 
@@ -53,13 +92,23 @@ public class DjActivity extends AbstractActivity implements DjView.Presenter {
 
     @Override
     public void search(String keywords) {
-        // TODO Auto-generated method stub
-        
+        DjPlace place = new DjPlace();
+        place.setKeywords(keywords);
+
+        placeController.goTo(place);
     }
 
     @Override
     public void addFacet(String facetId) {
-        // TODO Auto-generated method stub
-        
+        if (facetIds == null) {
+            facetIds = new ArrayList<String>();
+        }
+        facetIds.add(facetId);
+
+        DjPlace place = new DjPlace();
+        place.setKeywords(keywords);
+        place.setFacetIds(facetIds);
+
+        placeController.goTo(place);
     }
 }
