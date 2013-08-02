5c87dafb6105500221a3084694d7a4b342353925
KolonelKustard <ralph@totalchange.com>
2013-08-02 16:31:00 +0100

    Corrected a warning

--- discodj-web/src/main/java/com/totalchange/discodj/web/client/views/impl/FacetsWidget.java
+++ discodj-web/src/main/java/com/totalchange/discodj/web/client/views/impl/FacetsWidget.java
@@ -9,7 +9,6 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.totalchange.discodj.web.client.views.DjView;
