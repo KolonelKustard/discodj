c224d7e9c9c846baa5b40af542581c0d94f07b7e
KolonelKustard <ralph@totalchange.com>
2014-04-30 18:03:17 +0100

    Implementing the iterator syncing - tests all passing

--- discodj-core/src/main/java/com/totalchange/discodj/populator/IteratorComparator.java
+++ discodj-core/src/main/java/com/totalchange/discodj/populator/IteratorComparator.java
@@ -8,6 +8,55 @@ class IteratorComparator {
     ActionsToTake compare(Iterator<Catalogue.CatalogueEntity> src,
             Iterator<Catalogue.CatalogueEntity> dest) {
         ActionsToTake actionsToTake = new ActionsToTake();
+
+        if (notYetAtEnd(src, dest, actionsToTake)) {
+            compare(src, dest, src.next(), dest.next(), actionsToTake);
+        }
+
         return actionsToTake;
+    }
+
+    private boolean notYetAtEnd(Iterator<Catalogue.CatalogueEntity> src,
+            Iterator<Catalogue.CatalogueEntity> dest,
+            ActionsToTake actionsToTake) {
+        if (!src.hasNext()) {
+            while (dest.hasNext()) {
+                actionsToTake.delete(dest.next().getId());
+            }
+            return false;
+        } else if (!dest.hasNext()) {
+            while (src.hasNext()) {
+                actionsToTake.add(src.next().getId());
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void compare(Iterator<Catalogue.CatalogueEntity> src,
+            Iterator<Catalogue.CatalogueEntity> dest,
+            Catalogue.CatalogueEntity currSrc,
+            Catalogue.CatalogueEntity currDest, ActionsToTake actionsToTake) {
+        int compare = currSrc.getId().compareTo(currDest.getId());
+
+        if (compare > 0) {
+            actionsToTake.delete(currDest.getId());
+            if (dest.hasNext()) {
+                compare(src, dest, currSrc, dest.next(), actionsToTake);
+            }
+        } else if (compare < 0) {
+            actionsToTake.add(currSrc.getId());
+            if (src.hasNext()) {
+                compare(src, dest, src.next(), currDest, actionsToTake);
+            }
+        } else if (!currSrc.getLastModified()
+                .equals(currDest.getLastModified())) {
+            actionsToTake.update(currSrc.getId());
+        }
+
+        if (notYetAtEnd(src, dest, actionsToTake)) {
+            compare(src, dest, src.next(), dest.next(), actionsToTake);
+        }
     }
 }
