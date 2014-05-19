6a790ad9b11a4629026f31d35c5c02af48442ad6
KolonelKustard <ralph@totalchange.com>
2014-05-19 22:46:04 +0100

    Playing with making a custom Hamcrest matcher for SolrInputDocument

--- discodj-solr/src/test/java/com/totalchange/discodj/search/solr/SolrSearchPopulatorImplTests.java
+++ discodj-solr/src/test/java/com/totalchange/discodj/search/solr/SolrSearchPopulatorImplTests.java
@@ -9,8 +9,12 @@ import java.util.Collection;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.common.SolrInputDocument;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
@@ -37,28 +41,41 @@ public class SolrSearchPopulatorImplTests {
                     "Test Genre " + num, num, "Test Requested By " + num));
         }
         populator.commit();
+
+        for (int num = 0; num < numToAdd; num++) {
+            verify(solrServer).add(argThat(docMatcher(num)));
+        }
+        verify(solrServer).commit();
     }
 
-    private int assertDocs(Collection<SolrInputDocument> docs, int numAdded) {
-        for (SolrInputDocument doc : docs) {
-            numAdded++;
-            final int copyOfNum = numAdded;
-            assertEquals("Test ID " + copyOfNum,
-                    doc.getFieldValue(SolrSearchProviderImpl.F_ID));
-            assertEquals("Test Artist " + copyOfNum,
-                    doc.getFieldValue(SolrSearchProviderImpl.F_ARTIST));
-            assertEquals("Test Album " + copyOfNum,
-                    doc.getFieldValue(SolrSearchProviderImpl.F_ALBUM));
-            assertEquals("Test Genre " + copyOfNum,
-                    doc.getFieldValue(SolrSearchProviderImpl.F_GENRE));
-            assertEquals("Test Requested By " + copyOfNum,
-                    doc.getFieldValue(SolrSearchProviderImpl.F_REQUESTED_BY));
-            assertEquals(copyOfNum,
-                    doc.getFieldValue(SolrSearchProviderImpl.F_YEAR));
-            assertEquals("Test Title " + copyOfNum,
-                    doc.getFieldValue(SolrSearchProviderImpl.F_TITLE));
-        }
-        return numAdded;
+    private Matcher<SolrInputDocument> docMatcher(final int num) {
+        return new ArgumentMatcher<SolrInputDocument>() {
+            @Override
+            public boolean matches(Object item) {
+                SolrInputDocument doc = (SolrInputDocument) item;
+                System.out.println(num + ": " + doc);
+                return doc.getFieldValue(SolrSearchProviderImpl.F_ID).equals(
+                        "Test ID " + num)
+                        && doc.getFieldValue(SolrSearchProviderImpl.F_ARTIST)
+                                .equals("Test Artist " + num)
+                        && doc.getFieldValue(SolrSearchProviderImpl.F_ALBUM)
+                                .equals("Test Album " + num)
+                        && doc.getFieldValue(SolrSearchProviderImpl.F_GENRE)
+                                .equals("Test Genre " + num)
+                        && doc.getFieldValue(
+                                SolrSearchProviderImpl.F_REQUESTED_BY).equals(
+                                "Test Requested By " + num)
+                        && doc.getFieldValue(SolrSearchProviderImpl.F_YEAR)
+                                .equals(String.valueOf(num))
+                        && doc.getFieldValue(SolrSearchProviderImpl.F_TITLE)
+                                .equals("Test Title " + num);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Test SOLR Document number " + num);
+            }
+        };
     }
 
     private Media makeFakeMedia(final String id, final String artist,
