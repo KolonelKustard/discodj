d8d09301daca3b0236ea824d5e8ae26870857491
KolonelKustard <ralph@totalchange.com>
2014-05-20 21:43:20 +0100

    Srarted implementing SOLR iterator for sync-ing

--- discodj-solr/src/main/java/com/totalchange/discodj/search/solr/SolrCatalogueEntityIterator.java
+++ discodj-solr/src/main/java/com/totalchange/discodj/search/solr/SolrCatalogueEntityIterator.java
@@ -2,9 +2,40 @@ package com.totalchange.discodj.search.solr;
 
 import java.util.Iterator;
 
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+
 import com.totalchange.discodj.catalogue.Catalogue.CatalogueEntity;
+import com.totalchange.discodj.search.SearchException;
 
 public class SolrCatalogueEntityIterator implements Iterator<CatalogueEntity> {
+    private static final int NUM_PER_PAGE = 10000;
+
+    private SolrServer solrServer;
+
+    private int currentPage = 0;
+    private int nextOffset;
+
+    public SolrCatalogueEntityIterator(SolrServer solrServer) {
+        this.solrServer = solrServer;
+    }
+
+    private void getNextLoadFromSolr() throws SolrSearchException {
+        SolrQuery sq = new SolrQuery();
+        sq.setQuery("*:*");
+        sq.setSort(SolrSearchProviderImpl.F_ID, SolrQuery.ORDER.asc);
+        sq.setStart(currentPage * NUM_PER_PAGE);
+        sq.setRows(NUM_PER_PAGE);
+
+        try {
+            QueryResponse res = solrServer.query(sq);
+            res.getResults();
+        } catch (SolrServerException sEx) {
+            throw new SolrSearchException(sEx);
+        }
+    }
 
     @Override
     public boolean hasNext() {
@@ -13,7 +44,7 @@ public class SolrCatalogueEntityIterator implements Iterator<CatalogueEntity> {
     }
 
     @Override
-    public CatalogueEntity next() {
+    public CatalogueEntity next() throws SearchException {
         // TODO Auto-generated method stub
         return null;
     }
@@ -22,5 +53,4 @@ public class SolrCatalogueEntityIterator implements Iterator<CatalogueEntity> {
     public void remove() {
         throw new UnsupportedOperationException();
     }
-
 }
