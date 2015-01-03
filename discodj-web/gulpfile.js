fe076acb49673b40d3ceeb0793fda2adb44741e5
Ralph Jones <ralph@totalchange.com>
2015-01-03 17:57:06 +0000

    Trying out usemin but it'll be too clunky and complicated so going to swap to browserify

--- discodj-web/gulpfile.js
+++ discodj-web/gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require("gulp");
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
 
 gulp.task("default", function() {
-  // place code for your default task here
+  gulp.src('./src/main/webapp/*.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat'],
+      html: [minifyHtml({empty: true})],
+      js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('./src/main/webapp/build/'));
 });
 
 gulp.task("test", function() {
