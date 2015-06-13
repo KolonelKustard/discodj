5369ddd0e536d218523c55246f4cf24d686cfbdd
Ralph Jones <raljones@expedia.com>
2015-06-13 13:57:24 +0100

    Using subshell to execute git commands

--- discodj-debian/apt-deploy.sh
+++ discodj-debian/apt-deploy.sh
@@ -3,7 +3,7 @@
 GHPAGES_LOCAL=./discodj-gh-pages
 
 if [ -d "$GHPAGES_LOCAL" ]; then
-  git --work-tree=$GHPAGES_LOCAL --git-dir=$GHPAGES_LOCAL/.git pull origin gh-pages
+  (cd $GHPAGES_LOCAL && git pull origin gh-pages)
 else
   git clone -b gh-pages --single-branch https://github.com/KolonelKustard/discodj $GHPAGES_LOCAL
 fi
