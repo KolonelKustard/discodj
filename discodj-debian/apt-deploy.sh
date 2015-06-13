7a19f615c4cb3241df1417418fae5abee9bd0315
Ralph Jones <raljones@expedia.com>
2015-06-13 13:47:05 +0100

    Corrected dir check

--- discodj-debian/apt-deploy.sh
+++ discodj-debian/apt-deploy.sh
@@ -2,7 +2,7 @@
 
 GHPAGES_LOCAL=./discodj-gh-pages
 
-if [ -d "$REPO_HOME" ]; then
+if [ -d "$GHPAGES_LOCAL" ]; then
   git --work-tree=$GHPAGES_LOCAL pull origin gh-pages
 else
   git clone -b gh-pages --single-branch https://github.com/KolonelKustard/discodj $GHPAGES_LOCAL
