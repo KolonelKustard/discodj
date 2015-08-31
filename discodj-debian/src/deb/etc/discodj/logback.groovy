13f309658e04b642c6399d29cfe2af2766704f60
Ralph Jones <raljones@expedia.com>
2015-08-31 18:30:00 +0100

    Changed discodj-service log output to file

--- discodj-debian/src/deb/etc/discodj/logback.groovy
+++ discodj-debian/src/deb/etc/discodj/logback.groovy
@@ -1,13 +1,31 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.rolling.FixedWindowRollingPolicy
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy
 
 import static ch.qos.logback.classic.Level.*
 
-appender("STDOUT", ConsoleAppender) {
+appender("FILE", RollingFileAppender) {
+  file = "/var/log/discodj/discodj-service.log"
+
+  rollingPolicy(FixedWindowRollingPolicy) {
+    fileNamePattern = "discodj-service.%i.log"
+    minIndex = 1
+    maxIndex = 3
+  }
+
+  triggeringPolicy(SizeBasedTriggeringPolicy) {
+    maxFileSize = "1MB"
+  }
+
   encoder(PatternLayoutEncoder) {
     pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
   }
 }
 
+appender("ASYNC", AsyncAppender) {
+  appenderRef("FILE")
+}
+
 logger("com.totalchange", INFO)
-root(WARN, ["STDOUT"])
+root(WARN, ["ASYNC"])
