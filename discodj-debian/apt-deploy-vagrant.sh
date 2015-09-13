d314a3587e74d8e368a67b755d5a1ce164172af7
Ralph Jones <raljones@expedia.com>
2015-09-13 22:22:59 +0100

    Sorted deploy of apt and tweaking logging

--- discodj-debian/apt-deploy-vagrant.sh
+++ discodj-debian/apt-deploy-vagrant.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-vagrant up
-vagrant ssh -c '/vagrant/apt-deploy.sh '$1
-vagrant halt
+if vagrant up; then
+  if vagrant ssh -c '/vagrant/apt-deploy.sh '$1; then
+    vagrant halt
+  else
+    vagrant destroy -f
+    exit 1
+  fi
+else
+  vagrant destroy -f
+  exit 1
+fi
--- discodj-debian/apt-deploy.sh
+++ discodj-debian/apt-deploy.sh
@@ -4,6 +4,7 @@ set -e
 
 VERSION=${1/-/\~}
 WORKING_DIR=.
+GIT_URL=git@github.com:KolonelKustard/discodj.git
 GHPAGES_LOCAL=$WORKING_DIR/discodj-gh-pages
 DEB=/vagrant/target/discodj-debian\_$VERSION\_all.deb
 
@@ -18,14 +19,17 @@ if [ -d "$GHPAGES_LOCAL" ]; then
   echo Updating $GHPAGES_LOCAL
   (cd $GHPAGES_LOCAL && git pull origin gh-pages)
 else
-  echo Cloning gh-pages to $GHPAGES_LOCAL
-  git clone -b gh-pages --single-branch https://github.com/KolonelKustard/discodj $GHPAGES_LOCAL
+  echo Cloning gh-pages from $GIT_URL to $GHPAGES_LOCAL
+  git clone -b gh-pages --single-branch --depth=1 $GIT_URL $GHPAGES_LOCAL
 fi
+
+echo Removing pre-existing version\(s\) from apt repo
+reprepro -b $GHPAGES_LOCAL/apt-repo remove discodj discodj
 
 echo Adding $DEB to apt repo
 reprepro -b $GHPAGES_LOCAL/apt-repo includedeb discodj $DEB
 
 echo Pushing changes to gh-pages
 (cd $GHPAGES_LOCAL/apt-repo && git add .)
-(cd $GHPAGES_LOCAL && git commit -m "Deploying APT deb for version $VERSION")
+(cd $GHPAGES_LOCAL && git commit -m "Deploying APT deb for version $VERSION" --author="Maven Deploy <noreply@totalchange.com>")
 (cd $GHPAGES_LOCAL && git push origin gh-pages)
