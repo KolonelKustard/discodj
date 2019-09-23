b413518a24ca8390f332c10790d88b3d9b02edee
Ralph Jones <4283551+KolonelKustard@users.noreply.github.com>
2019-09-23 14:42:53 +0100

    Unused import

--- catalogue/src/main/java/com/totalchange/discodj/catalogue/sync/MediaEntitySyncHandler.java
+++ catalogue/src/main/java/com/totalchange/discodj/catalogue/sync/MediaEntitySyncHandler.java
@@ -1,7 +1,5 @@
 package com.totalchange.discodj.catalogue.sync;
 
-import com.totalchange.discodj.server.media.Media;
-
 public interface MediaEntitySyncHandler {
     void add(String id);
     void update(String id);
