da00dee796a68c922bb5b240978d7a241130b024
Ralph Jones <4283551+KolonelKustard@users.noreply.github.com>
2019-09-23 14:43:56 +0100

    Unused import

--- catalogue/src/main/java/com/totalchange/discodj/catalogue/sync/MediaEntitySync.java
+++ catalogue/src/main/java/com/totalchange/discodj/catalogue/sync/MediaEntitySync.java
@@ -2,7 +2,6 @@ package com.totalchange.discodj.catalogue.sync;
 
 import com.totalchange.discodj.server.media.MediaEntity;
 
-import java.util.Iterator;
 import java.util.List;
 
 public final class MediaEntitySync {
