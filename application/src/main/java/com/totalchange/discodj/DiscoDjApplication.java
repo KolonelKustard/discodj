3e7388e3a404454a5a0b82d5bbe1217d03e91017
Ralph Jones <4283551+KolonelKustard@users.noreply.github.com>
2020-01-16 15:02:02 +0000

    Staring a base application module

--- application/src/main/java/com/totalchange/discodj/DiscoDjApplication.java
+++ application/src/main/java/com/totalchange/discodj/DiscoDjApplication.java
@@ -1,4 +1,22 @@
 package com.totalchange.discodj;
 
+import com.totalchange.discodj.catalogue.Catalogue;
+import com.totalchange.discodj.media.file.FileMediaSource;
+import com.totalchange.discodj.search.lucene.LuceneSearchProvider;
+import com.totalchange.discodj.server.media.MediaSource;
+import com.totalchange.discodj.server.search.SearchProvider;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class DiscoDjApplication {
+    public static void main(String[] args) throws IOException {
+        final SearchProvider searchProvider = new LuceneSearchProvider(
+                Paths.get("/Users/raljones/tdev/discodj-search-index"));
+        final Catalogue catalogue = new Catalogue(
+                new MediaSource[] { new FileMediaSource(Paths.get("/Users/raljones/tdev/discodj-sample-media")) },
+                searchProvider);
+
+
+    }
 }
