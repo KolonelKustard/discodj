350868f65a1f248ed7f49067c396feb7e5e20fef
KolonelKustard <KolonelKustard@users.noreply.github.com>
2020-08-26 22:25:35 +0100

    Crappy initial version of search

--- requests-ui/src/App.tsx
+++ requests-ui/src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Grid from '@material-ui/core/Grid';
+import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Result {
+  title: string
 }
 
-export default App;
+export default function BasicTextFields() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [results, setResults] = useState<Result[]>([]);
+
+  useEffect(() => { fetch("/search")
+    .then(res => res.json())
+    .then((result) => {
+        setIsLoaded(true);
+        setResults(result.results);
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )}, []);
+
+  return (
+  <div>
+    <Grid container spacing={1} alignItems="flex-end">
+      <Grid item>
+        <SearchIcon />
+      </Grid>
+      <Grid item>
+        <TextField id="search" label="Search tracks" />
+      </Grid>
+    </Grid>
+    <ul>
+      {results.map(item => (
+        <li key={item.title}>
+          {item.title}
+        </li>
+      ))}
+    </ul>
+</div>
+  );
+}
