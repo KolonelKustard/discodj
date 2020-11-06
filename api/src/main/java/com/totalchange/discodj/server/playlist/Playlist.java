d38798a85eef97094eced8bf491fddd8c71b4445
Ralph Jones <4283551+KolonelKustard@users.noreply.github.com>
2020-11-06 12:20:54 +0000

    Moving playlist management about

--- api/src/main/java/com/totalchange/discodj/server/playlist/Playlist.java
+++ api/src/main/java/com/totalchange/discodj/server/playlist/Playlist.java
@@ -2,8 +2,12 @@ package com.totalchange.discodj.server.playlist;
 
 import com.totalchange.discodj.server.media.Media;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface Playlist {
     void add(Media mediaToAdd);
-    Media pop();
-    Media getNowPlaying();
+    Optional<Media> pop();
+    Optional<Media> getNowPlaying();
+    List<Media> getPlaylist();
 }
--- playlist/src/main/java/com/totalchange/discodj/playlist/PlaylistQueue.java
+++ playlist/src/main/java/com/totalchange/discodj/playlist/PlaylistQueue.java
@@ -13,50 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.totalchange.discodj.queue;
+package com.totalchange.discodj.playlist;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.Optional;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
+import com.totalchange.discodj.server.media.Media;
+import com.totalchange.discodj.server.playlist.Playlist;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.totalchange.discodj.catalogue.Catalogue;
-import com.totalchange.discodj.media.Media;
-
-@Singleton
-public class PlaylistQueue {
+public class PlaylistQueue implements Playlist {
     private static final int POPPED_LIST_SIZE = 20;
 
     private static final Logger logger = LoggerFactory
             .getLogger(PlaylistQueue.class);
 
-    private final Queue<Media> defaultQueue;
     private final List<Media> requestedQueue = new ArrayList<>();
     private final List<String> poppedIdList = new ArrayList<>(POPPED_LIST_SIZE);
 
-    private Catalogue catalogue;
-    private Media lastPopped = null;
-
-    @Inject
-    public PlaylistQueue(Catalogue catalogue) {
-        logger.trace("Creating new queue");
-        this.catalogue = catalogue;
-
-        logger.trace("Populating default playlist from catalogue");
-        defaultQueue = new LinkedList<>(catalogue.getDefaultPlaylist());
-        logger.trace("Done");
-    }
-
-    private Media fetchMedia(String id) {
-        // TODO Add caching of media
-        return catalogue.getMedia(id);
-    }
+    private Optional<Media> nowPlaying = Optional.empty();
 
     private void addPoppedId(String id) {
         if (poppedIdList.size() >= POPPED_LIST_SIZE) {
@@ -69,7 +46,20 @@ public class PlaylistQueue {
         return poppedIdList.contains(media.getId());
     }
 
-    public synchronized Media pop() {
+    @Override
+    public synchronized void add(Media media) {
+        logger.trace("Adding {} to playlist", media);
+
+        if (!skipIfInPoppedList(media) && !requestedQueue.contains(media)) {
+            requestedQueue.add(media);
+        } else {
+            logger.trace("Skipping adding {} as is in already in playlist "
+                    + "or has already been played", media);
+        }
+    }
+
+    @Override
+    public synchronized Optional<Media> pop() {
         logger.trace("Popping next item off the queue - trying requested "
                 + "queue first");
 
@@ -80,59 +70,28 @@ public class PlaylistQueue {
                     logger.trace("Skipping already played media {}", nextMedia);
                     return pop();
                 } else {
-                    lastPopped = nextMedia;
-                    addPoppedId(lastPopped.getId());
+                    nowPlaying = Optional.of(nextMedia);
+                    addPoppedId(nextMedia.getId());
                     logger.trace("Returning media from requested queue {}",
-                            lastPopped);
-                    return lastPopped;
+                            nowPlaying);
+                    return nowPlaying;
                 }
             }
         }
 
-        logger.trace("Nothing in requested queue, trying default queue");
-        Media nextMedia = defaultQueue.poll();
-        if (nextMedia != null) {
-            if (skipIfInPoppedList(nextMedia)) {
-                logger.trace("Skipping already played media {}", nextMedia);
-                return pop();
-            } else {
-                lastPopped = nextMedia;
-                addPoppedId(lastPopped.getId());
-                logger.trace("Returning media from default queue {}",
-                        lastPopped);
-                return lastPopped;
-            }
-        }
-
-        lastPopped = null;
-        logger.trace("Nothing left in the queue - returning null");
-        return lastPopped;
+        nowPlaying = Optional.empty();
+        logger.trace("Nothing left in the queue - returning empty");
+        return nowPlaying;
     }
 
-    public Media getLastPopped() {
-        return lastPopped;
+    @Override
+    public Optional<Media> getNowPlaying() {
+        return nowPlaying;
     }
 
+    @Override
     public synchronized List<Media> getPlaylist() {
         return new ArrayList<>(requestedQueue);
-    }
-
-    public synchronized void push(String id) {
-        logger.trace("Adding {} to playlist", id);
-
-        try {
-            Media media = fetchMedia(id);
-            if (!skipIfInPoppedList(media) && !requestedQueue.contains(media)) {
-                requestedQueue.add(media);
-            } else {
-                logger.trace("Skipping adding {} as is in already in playlist "
-                        + "or has already been played", media);
-            }
-        } catch (Exception ex) {
-            logger.warn("Failed to add id " + id
-                    + " with fetch from catalogue error " + ex.getMessage(),
-                    ex);
-        }
     }
 
     public synchronized int getWhenCanBePlayedAgain(Media media) {
